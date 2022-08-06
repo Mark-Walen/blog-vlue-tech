69e74f4f36c23b53f303e5f224f2cf5f1031787a
Mark-Walen <walen.mark2509758@gmail.com>
2022-08-07 00:58:40 +0800

    add pwa function

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,20 @@ const moment = require('moment');
 
 module.exports = {
     title: 'Blue Monster',
-    description: 'Just playing around',
+    description: '在此小栈喝盏茶，小憩片刻，汲取知识。',
+    head: [
+        ['link', { rel: 'icon', href: '/favicon.ico' }],
+        ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+        ['meta', { name: 'author', content: 'Mark Walen' }],
+        ['meta', { name: 'keywords', content: '蓝芒小栈, Blue Monster, 嵌入式软件, STM32, C 语言, 数据结构' }],
+        ['meta', { name: 'theme-color', content: '#1e2d51' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+        ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#1e2d51' }],
+        ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+    ],
     themeConfig: {
         nav: navbar,
         sidebar: 'auto',
@@ -31,7 +44,14 @@ module.exports = {
         '@vuepress/last-updated': {
             transformer: (timestamp, lang) => {
                 moment.locale(lang)
-                return moment(timestamp).fromNow('LLL')
+                return moment(timestamp).format('LLL')
+            }
+        },
+        '@vuepress/pwa': {
+            ServiceWorker: true,
+            updatePopup: {
+                message: "发现心内容可用",
+                buttonText: "刷新"
             }
         }
     }
